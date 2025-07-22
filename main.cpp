b536d172d7be69b7ac993d9981fc5e4c00428225
pluswing <toshiyuki@pluswing.co.jp>
2025-07-22 10:15:37 +0900

    #003 モデル描画実装中。

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
+#include <vector>
+#include <cmath>
 #include "tgaimage.h"
+#include "model.h"
+#include "geometry.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
+Model *model = NULL;
+const int width = 800;
+const int height = 800;
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
   bool steep = false;
@@ -17,31 +24,52 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 
   int dx = x1-x0;
   int dy = y1-y0;
-  float derror = std::abs(dy / float(dx));
-  float error = 0;
+  int derror2 = std::abs(dy) * 2;
+  int error2 = 0;
   int y = y0;
-  for (int x = x0; x <= x1; x++) {
-    if (steep) {
+  if (steep) {
+    for (int x = x0; x <= x1; x++) {
       image.set(y, x, color);
-    } else {
-      image.set(x, y, color);
+      error2 += derror2;
+      if (error2 > dx) {
+        y += (y1 > y0 ? 1 : -1);
+        error2 -= dx*2;
+      }
     }
-    error += derror;
-    if (error > 0.5) {
-      y += (y1 > y0 ? 1 : -1);
-      error -= 1.0;
+  } else {
+    for (int x = x0; x <= x1; x++) {
+      image.set(x, y, color);
+      error2 += derror2;
+      if (error2 > dx) {
+        y += (y1 > y0 ? 1 : -1);
+        error2 -= dx*2;
+      }
     }
   }
 }
 
-int main (int argc, char** argv) {
-  TGAImage image(100, 100, TGAImage::RGB);
-  line(13, 20, 80, 40, image, white);
-  line(20, 13, 40, 80, image, red);
-  line(80, 40, 13, 20, image, red);
-  // line(10, 80, 80, 60, image, white);
-  // image.set(52, 41, red);
+int main(int argc, char** argv) {
+  if (2 == argc) {
+    model = new Model(argv[1]);
+  } else {
+    model = new Model("obj/african_head.obj");
+  }
+  TGAImage image(width, height, TGAImage::RGB);
+  for (int i=0; i<model->nfaces(); i++) {
+    std::vector<int> face = model->face(i);
+    for (int j=0; j < 3; j++) {
+      Vec3f v0 = model->vert(face[j]);
+      Vec3f v1 = model->vert(face[j+1]%3);
+      int x0 = (v0.x + 1.0) * width / 2.0;
+      int y0 = (v0.y + 1.0) * height / 2.0;
+      int x1 = (v1.x + 1.0) * width / 2.0;
+      int y1 = (v1.y + 1.0) * height / 2.0;
+      line(x0, y0, x1, y1, image, white);
+    }
+  }
+
   image.flip_vertically();
   image.write_tga_file("output.tga");
+  delete model;
   return 0;
 }
