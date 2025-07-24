b9445cac1b0c48ef3d0c54ad1a9d8edd0134e815
pluswing <toshiyuki@pluswing.co.jp>
2025-07-24 10:23:29 +0900

    #004 model.h, model.cppの写経中。

--- geometry.h
+++ geometry.h
@@ -36,12 +36,12 @@ template <class t> struct Vec3 {
 	inline Vec3<t> operator -(const Vec3<t> &v) const { return Vec3<t>(x-v.x, y-v.y, z-v.z); }
 	inline Vec3<t> operator *(float f)          const { return Vec3<t>(x*f, y*f, z*f); }
 	inline t       operator *(const Vec3<t> &v) const { return x*v.x + y*v.y + z*v.z; }
-	float norm () const {
+	float norm() const {
     return std::sqrt(
       x * x + y * y + z * z
     );
   }
-  Vec3<t> notmalize(t l=1) {
+  Vec3<t> normalize(t l=1) {
     *this = (*this)*(l/norm());
     return *this;
   }
