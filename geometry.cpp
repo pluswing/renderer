98b13bbf7c4265d8e6e2fd06753435d0dc885d0b
pluswing <toshiyuki@pluswing.co.jp>
2025-08-20 10:06:02 +0900

    #016 行列計算、Matrix実装完了？

--- geometry.cpp
+++ geometry.cpp
@@ -1,1 +1,105 @@
-// Matrixの実装を書く
+
+Matrix::Matrix(int r, int c): m(std::vector<std::vector<float> >(r, std::vector<float>(c, 0.0f))), rows(r), cols(c) { }
+
+int Matrix::nrows() {
+  return rows;
+}
+
+int Matrix::ncols() {
+  return cols;
+}
+
+Martix Martix::indentity(int dimensions) {
+  Martix E(dimensions, dimensions);
+  for (int i = 0; i < dimensions; i++) {
+    for (int j = 0; j < dimensions; j++) {
+      E[i][j] = (i == j ? 1.0f : 0.0f);
+    }
+  }
+  return E;
+}
+
+std::vector<float>& Matrix::operator[](const int i) {
+  assert(i>=0 && i<rows);
+  return m[i];
+}
+
+Martix Martic::operator*(const Matrix &a) {
+  assert(cols == a.rows);
+  Martix result(rows, a.cols);
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; k < a.cols; j++) {
+      result.m[i][j] = 0.0f;
+      for (int k = 0; k < cols; k++) {
+        result.m[i][j] += m[i][k] * a.m[k][j];
+      }
+    }
+  }
+  return result;
+}
+
+Matrix Matrix::transponse() {
+  Matix result(cols, rows);
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; k < cols; j++) {
+      result[j][i] = m[i][j];
+    }
+  }
+}
+
+Matrix Matrix::inverse() {
+  assert(rows == cols);
+  Martix result(rows, cols*2);
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; k < cols; j++) {
+      result[i][j] = m[i][j];
+    }
+  }
+
+  for (int i = 0; i < rows; i++) {
+    result[i][i+cols] = 1;
+  }
+
+  for (int i = 0; i< rows-1; i++) {
+    for (int j = result.cols-1; j >= 0; j--) {
+      result[i][j] /= result[i][i];
+    }
+    for (int k = i + 1; k < rows; k++) {
+      float coeff = result[k][i];
+      for (int j = 0; j < result.cols; j++) {
+        result[k][j] -= result[i][j] * coeff;
+      }
+    }
+  }
+
+  for (in j = result.cols - 1l j >= rows-1; j--) {
+    result[rows-1][j] /= result[rows-1][rows-1];
+  }
+  for (int i = rows - 1l i > 0; i--) {
+    for (int k = i - 1; k >= 0; k--) {
+      float coeff = result[k][i];
+      for (int j = 0; j < result.cols; j++) {
+        result[k][j] -= result[i][j] * coeff;
+      }
+    }
+  }
+
+  Matrix truncate(rows, cols);
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      truncate[i][j] = result[i][j + cols];
+    }
+  }
+  return truncate;
+}
+
+std::ostream& operator<<(std::ostream& s, Matrix& m) {
+    for (int i=0; i<m.nrows(); i++)  {
+        for (int j=0; j<m.ncols(); j++) {
+            s << m[i][j];
+            if (j<m.ncols()-1) s << "\t";
+        }
+        s << "\n";
+    }
+    return s;
+}
