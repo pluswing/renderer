217bbfae2e8356ce1c81c7f77a75eeb7e4a1b680
pluswing <toshiyuki@pluswing.co.jp>
2025-09-02 10:07:38 +0900

    #023 model.h写経完了 （テクスチャ読み込み実装）

--- model.cpp
+++ model.cpp
@@ -4,8 +4,9 @@
 #include <sstream>
 #include <vector>
 #include "model.h"
+#include "tgaimage.h"
 
-Model::Model(const char *filename): verts_(), faces_(), texture_verts_(), texture_faces_() {
+Model::Model(const char *filename): verts_(), faces_(), norms_(), uv_() {
   std::ifstream in;
   in.open(filename, std::ifstream::in);
   if (in.fail()) return;
@@ -21,30 +22,37 @@ Model::Model(const char *filename): verts_(), faces_(), texture_verts_(), textur
         iss >> v.raw[i];
       }
       verts_.push_back(v);
-    } else if (!line.compare(0, 3, "vt ")) {
-      iss >> trash;
-      Vec2f v;
-      for (int i = 0; i < 2; i++) {
-        iss >> v.raw[i];
+    } else if (!line.compare(0, 3, "vn ")) {
+      iss >> trash >> trash;
+      Vec3f n;
+      for (int i = 0; i < 3; i++) {
+        iss >> n.raw[i];
       }
-      texture_verts_.push_back(v);
+      norms_.push_back(n);
+    } else if (!line.compare(0, 3, "vt ")) {
+      iss >> trash >> trash;
+      Vec2f uv;
+      for (int i = 0; i < 2; i++) {
+        iss >> uv.raw[i];
+      }
+      uv_.push_back(uv);
     } else if (!line.compare(0, 2, "f ")) {
-      std::vector<int> f;
-      std::vector<int> t;
-      int itrash, idx, tidx;
+      std::vector<Vec3i> f;
+      Vec3i tmp;
       iss >> trash;
       // idx/tidx/x
-      while (iss >> idx >> trash >> tidx >> trash >> itrash) {
-        idx--;
-        tidx--;
-        f.push_back(idx);
-        t.push_back(tidx);
+      while (iss >> tmp.raw[0] >> trash >> tmp.raw[1] >> trash >> tmp.raw[2]) {
+        for (int i = 0; i < 3; i++) {
+          tmp.raw[i]--;
+        }
+        f.push_back(tmp);
       }
       faces_.push_back(f);
-      texture_faces_.push_back(t);
     }
   }
-  std::cerr << "# v# " << verts_.size() << " f# " << faces_.size() << " vt# " << texture_verts_.size() << std::endl;
+std::cerr << "# v# " << verts_.size() << " f# "  << faces_.size() << " vt# " << uv_.size() << " vn# " << norms_.size() << std::endl;
+
+  load_texture(filename, "_diffuse.tga", diffusemap_);
 }
 
 Model::~Model() {
@@ -59,17 +67,35 @@ int Model::nfaces() {
 }
 
 std::vector<int> Model::face(int idx) {
-  return faces_[idx];
-}
-
-std::vector<int> Model::texture_face(int idx) {
-  return texture_faces_[idx];
+  std::vector<int> face;
+  for (int i = 0; i < (int)faces_[idx].size(); i++) {
+    face.push_back(faces_[idx][i].raw[0]);
+  }
+  return face;
 }
 
 Vec3f Model::vert(int i) {
   return verts_[i];
 }
 
-Vec2f Model::texture_vert(int i) {
-  return texture_verts_[i];
+void Model::load_texture(std::string filename, const char *suffix, TGAImage &img) {
+  std::string texfile(filename);
+  size_t dot = texfile.find_last_of(".");
+  if (dot != std::string::npos) {
+    texfile = texfile.substr(0, dot) + std::string(suffix);
+    std::cerr << "texture file " << texfile << " loading " << (img.read_tga_file(texfile.c_str()) ? "ok" : "failed") << std::endl;
+    img.flip_vertically();
+  }
+}
+
+TGAColor Model::diffuse(Vec2i uv) {
+  return diffusemap_.get(uv.x, uv.y);
+}
+
+Vec2i Model::uv(int iface, int nvert) {
+  int idx = faces_[iface][nvert].raw[1];
+  return Vec2i(
+    uv_[idx].x * diffusemap_.get_width(),
+    uv_[idx].y * diffusemap_.get_height()
+  );
 }
--- model.h
+++ model.h
@@ -7,19 +7,20 @@
 class Model {
   private:
     std::vector<Vec3f> verts_;
-    std::vector<std::vector<int> > faces_;
-    std::vector<Vec2f> texture_verts_;
-    std::vector<std::vector<int> > texture_faces_;
+    std::vector<std::vector<Vec3i> > faces_;
+    std::vector<Vec3f> norms_;
+    std::vector<Vec2f> uv_;
+    TGAImage diffusemap_;
+    void load_texture(std::string filename, const char *suffix, TGAImage &img);
   public:
     Model(const char *filename);
     ~Model();
     int nverts();
     int nfaces();
     Vec3f vert(int i);
+    Vec2i uv(int iface, int nvert);
+    TGAColor diffuse(Vec2i uv);
     std::vector<int> face(int idx);
-
-    Vec2f texture_vert(int i);
-    std::vector<int> texture_face(int idx);
 };
 
 #endif
