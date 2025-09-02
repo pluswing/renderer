558e6161f82b14d4768678799497f2a18c3194b4
pluswing <toshiyuki@pluswing.co.jp>
2025-09-02 10:08:08 +0900

    #024 テクスチャ貼り付け対応版triangle関数の写経

--- main.cpp
+++ main.cpp
@@ -60,47 +60,64 @@ void line(Vec3i p0, Vec3i p1, TGAImage &image, TGAColor color) {
     }
   }
 }
-/*
-void triangle(Vec3f *pts, float *zbuffer, TGAImage &image, Vec2f *texture_pts, TGAImage &texture) {
-  Vec2f bboxmin(
-    std::numeric_limits<float>::max(),
-    std::numeric_limits<float>::max()
-  );
-  Vec2f bboxmax(
-    -std::numeric_limits<float>::max(),
-    -std::numeric_limits<float>::max()
-  );
-  Vec2f clamp(image.get_width() - 1, image.get_height() - 1);
 
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 2; j++) {
-      bboxmin.raw[j] = std::max(0.0f, std::min(bboxmin.raw[j], pts[i].raw[j]));
-      bboxmax.raw[j] = std::max(clamp.raw[j], std::min(bboxmax.raw[j], pts[i].raw[j]));
-    }
+void triangle(
+  Vec3i t0, Vec3i t1, Vec3i t2,
+  Vec2i uv0, Vec2i uv1, Vec2i uv2,
+  TGAImage &image, float intensity, int *zbuffer) {
+
+  if (t0.y == t1.y && t0.y == t2.y) return;
+
+  if (t0.y > t1.y) {
+    std::swap(t0, t1);
+    std::swap(uv0, uv1);
   }
-  Vec3f P;
-  for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++) {
-    for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++) {
-      Vec3f bc_screen = barycentric(pts[0], pts[1], pts[2], P);
-      if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) {
-        continue;
-      }
-      P.z = 0;
-      for (int i = 0; i < 3; i++) {
-        P.z += pts[i].z * bc_screen.raw[i];
-      }
-      // FIXME 重心からテクスチャのx,yを求める
-      int tx = 0;
-      int ty = 0;
-      if (zbuffer[int(P.x + P.y * width)] < P.z) {
-        zbuffer[int(P.x + P.y * width)] = P.z;
-        int tx =
-        image.set(P.x, P.y, texture.get(tx, ty));
+
+  if (t0.y > t2.y) {
+    std::swap(t0, t2);
+    std::swap(uv0, uv2);
+  }
+
+  if (t1.y > t2.y) {
+    std::swap(t1, t2);
+    std::swap(uv1, uv2);
+  }
+
+  int total_height = t2.y - t0.y;
+  for (int i = 0; i < total_height; i++) {
+    bool second_half = i > t1.y - t0.y || t1.y == t0.y;
+    int segment_height = second_half ? t2.y - t1.y : t1.y - t0.y;
+    float alpha = (float) i / total_height;
+    float beta = (float) (i - (second_half ? t1.y - t0.y : 0)) / segment_height;
+    Vec3i A = t0 + Vec3f(t2-t0) * alpha;
+    Vec3i B = second_half ? t1 + Vec3f(t2 - t1) * beta
+                          : t0 + Vec3f(t1 - t0) * beta;
+    Vec2i uvA = uv0 + (uv2 - uv0) * alpha;
+    Vec2i uvB = second_half ? uv1 + (uv2 - uv1) * beta
+                            : uv0 + (uv1 - uv0) * beta;
+
+    if (A.x > B.x) {
+      std::swap(A, B);
+      std::swap(uvA, uvB);
+    }
+    for (int j = A.x; j <= B.x; j++) {
+      float phi = B.x == A.x ? 1.0f : (float)(j - A.x) / (float)(B.x - A.x);
+      Vec3i P = Vec3f(A) + Vec3f(B-A) * phi;
+      Vec2i uvP = uvA + (uvB - uvA) * phi;
+      int idx = P.x + P.y + width;
+      if (zbuffer[idx] > P.z) {
+        zbuffer[idx] = P.z;
+        TGAColor color = model->diffuse(uvP);
+        image.set(P.x, P.y, TGAColor(
+          color.r * intensity,
+          color.g * intensity,
+          color.b * intensity
+        ));
       }
     }
   }
 }
-*/
+
 /*
 void rasterize(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color, int ybuffer[]) {
 
@@ -229,7 +246,7 @@ int main(int argc, char** argv) {
         screen_coords[j] = m2v(ViewPort * Projection * v2m(v));
         world_coords[j] = v;
       }
-      Vec3f n = (world_coods[2] - world_coods[0]) ^ (world_coods[1] - world_coods[0]);
+      Vec3f n = (world_coords[2] - world_coords[0]) ^ (world_coords[1] - world_coords[0]);
       n.normalize();
       float intensity = n * light_dir;
       if (intensity > 0) {
