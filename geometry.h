952b38f311c61ed1dfc8f370dc9c632568b3c620
pluswing <toshiyuki@pluswing.co.jp>
2025-09-30 10:04:04 +0900

    #037 geometryの写経(2)

--- geometry.h
+++ geometry.h
@@ -9,7 +9,7 @@
 
 template<size_t DimCols, size_t DimRaws, typename T> class mat;
 
-template <size_t DIM, typename T> struct vec {
+template<size_t DIM, typename T> struct vec {
   vec() {
     for (size_t i = DIM; i--; data_[i] = T());
   }
@@ -27,11 +27,11 @@ private:
 
 /////////////////////////////////////////////////
 
-template <typename T> struct vec<2, T> {
+template<typename T> struct vec<2, T> {
   vec() : x(T()), y(T()) {}
   vec(T X, T Y) : x(X), y(Y) {}
 
-  template <class U> vec<2, T>(const vec<2, U> &v);
+  template<class U> vec<2, T>(const vec<2, U> &v);
 
   T& operator[](const size_t i) {
     assert(i < 2);
@@ -47,11 +47,11 @@ template <typename T> struct vec<2, T> {
 
 /////////////////////////////////////////////////
 
-template <typename T> struct vec<3, T> {
+template<typename T> struct vec<3, T> {
   vec() : x(T()), y(T()), z(T()) {}
   vec(T X, T Y, T Z) : x(X), y(Y), z(Z) {}
 
-  template <class U> vec<3, T>(const vec<3, U> &v);
+  template<class U> vec<3, T>(const vec<3, U> &v);
 
   T& operator[](const size_t i) {
     assert(i < 3);
@@ -66,5 +66,70 @@ template <typename T> struct vec<3, T> {
 
   T x, y, z;
 };
+
+/////////////////////////////////////////////////
+
+template<size_t DIM, typename T> T operator*(const vec<DIM, T>& lhs, const vec<DIM, T>& rhs) {
+  T ret = T();
+  for (size_t i = DIM; i--; ret += lhs[i] * rhs[i]);
+  return ret;
+}
+
+template<size_t DIM, typename T> vec<DIM, T> operator+(vec<DIM, T> lhs, const vec<DIM, T>& rhs) {
+  for (size_t i = DIM; i--; lhs[i] += rhs[i]);
+  return lhs;
+}
+
+template<size_t DIM, typename T> vec<DIM, T> operator-(vec<DIM, T> lhs, const vec<DIM, T>& rhs) {
+  for (size_t i = DIM; i--; lhs[i] -= rhs[i]);
+  return lhs;
+}
+
+template<size_t DIM, typename T, typename U> vec<DIM, T> operator*(vec<DIM, T> lhs, const U& rhs) {
+  for (size_t i = DIM; i--; lhs[i] *= rhs);
+  return lhs;
+}
+
+template<size_t DIM, typename T, typename U> vec<DIM, T> operator/(vec<DIM, T> lhs, const U& rhs) {
+  for (size_t i = DIM; i--; lhs[i] /= rhs);
+  return lhs;
+}
+
+template<size_t LEN, size_t DIM, typename T> vec<LEN, T> embed(const vec<DIM, T> &v, T fill = 1) {
+  vec<LEN, T> ret;
+  for (size_t i = LEN; i--; ret[i] = (i < DIM ? v[i] : fill));
+  return ret;
+}
+
+template<size_t LEN, size_t DIM, typename T> vec<LEN, T> proj(const vec<DIM, T> &v) {
+  vec<LEN, T> ret;
+  for (size_t i = LEN; i--; ret[i] = v[i]);
+  return ret;
+}
+
+template<size_t DIM, typename T> std::ostream& operator<<(std::ostream& out, vec<DIM, T> &v) {
+  for (size_t i = 0; i < DIM; i++) {
+    out << v[i] << " ";
+  }
+  return out;
+}
+
+/////////////////////////////////////////////////
+
+template<size_t DIM, typename T> struct dt {
+  static T det(const mat<DIM, DIM, T>& src) {
+    T ret = 0;
+    for (size_t i = DIM; i--; ret += src[0][i] * src.cofactor(0, i)):
+    return ret;
+  }
+};
+
+template<typename T> struct dt<1, T> {
+  struct T det(const mat<1, 1, T>& src) {
+    return src[0][0];
+  }
+};
+
+/////////////////////////////////////////////////
 
 #endif
